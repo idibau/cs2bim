22b5849f9da8f399153f945bd94afbff31ec5bee
Joel Gschwind <joel.gschwind@fhnw.ch>
2024-10-14 15:54:41 +0200

    fix typo

--- src/cs2bim/service/bounding_box.py
+++ src/cs2bim/service/bounding_box.py
@@ -8,24 +8,24 @@ class BoundingBox:
 
     Attributes
     ----------
-    min_nord : float
+    min_north : float
         Minimum longitude
     min_east : float
         Minimum latitude
-    max_nord : float
+    max_north : float
         Maximum longitude
     max_east : float
         Maximum latitude
     """
 
-    def __init__(self, min_nord: float, min_east: float, max_nord: float, max_east: float) -> None:
-        self.min_nord = min_nord
+    def __init__(self, min_north: float, min_east: float, max_north: float, max_east: float) -> None:
+        self.min_north = min_north
         self.min_east = min_east
-        self.max_nord = max_nord
+        self.max_north = max_north
         self.max_east = max_east
 
     def get_wgs84_bounding_box_as_string(self) -> str:
         transformer = Transformer.from_crs("epsg:2056", "epsg:4326")
-        p_1 = transformer.transform(self.min_east, self.min_nord)
-        p_2 = transformer.transform(self.max_east, self.max_nord)
+        p_1 = transformer.transform(self.min_east, self.min_north)
+        p_2 = transformer.transform(self.max_east, self.max_north)
         return f"{p_1[1]},{p_1[0]},{p_2[1]},{p_2[0]}"
