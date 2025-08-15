0d217d32de83b51430eae1d12e1898d2bf29c758
Joel Gschwind <joel.gschwind@fhnw.ch>
2025-08-15 12:27:22 +0200

    Update log file directory for better consistency

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -16,10 +16,9 @@ def setup_logger(log_file_name: str = None):
     root_logger.addHandler(stream_handler)
 
     if log_file_name is not None:
-        log_file_name = datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
-        app_logs_path = "/logs/app"
-        Path(app_logs_path).mkdir(parents=True, exist_ok=True)
-        file_handler = logging.FileHandler(f"{app_logs_path}/{log_file_name}.log")
+        logs_path = "/workspace/logs"
+        Path(logs_path).mkdir(parents=True, exist_ok=True)
+        file_handler = logging.FileHandler(f"{logs_path}/{log_file_name}.log")
         file_handler.setFormatter(logging.Formatter(logging_format))
         root_logger.addHandler(file_handler)
 
