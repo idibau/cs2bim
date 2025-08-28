1a6a9ac71b456eaa163f4e33a3ca2facb16904a8
Joel Gschwind <joel.gschwind@fhnw.ch>
2025-08-28 17:39:21 +0200

    Update field descriptions in GenerateModelRequest schema

--- src/api/generate_model_request.py
+++ src/api/generate_model_request.py
@@ -5,7 +5,7 @@ from core.ifc.model.ifc_version import IfcVersion
 
 
 class GenerateModelRequest(BaseModel):
-    IFC_VERSION: IfcVersion = Field(..., description="The IFC version")
+    IFC_VERSION: IfcVersion = Field(..., description="The IFC version [IFC4, IFC4x3]")
     NAME: str = Field(..., description="The name of the model")
-    POLYGON: str = Field(..., description="WKT string representing the polygon (must be closed)")
-    PROJECT_ORIGIN: Optional[str] = Field(None, description="Optional origin as comma-separated string 'x,y,z'")
+    POLYGON: str = Field(..., description="The closed WKT string representing the polygon")
+    PROJECT_ORIGIN: Optional[str] = Field(None, description="Optional origin as comma-separated string [x,y,z]")
