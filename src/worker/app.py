30de8dda20bd65f9adb8388f9a3da225463d2752
Joel Gschwind <joel.gschwind@fhnw.ch>
2025-09-02 09:00:20 +0200

    Fix logger setup for Celery worker processes

--- src/worker/app.py
+++ src/worker/app.py
@@ -1,7 +1,9 @@
 import logging
 import os
-from celery import Celery
+from celery import Celery, signals
+from celery.signals import worker_process_init
 
+from datetime import datetime
 from core.ifc.model.ifc_version import IfcVersion
 from core.model_generator import ModelGenerator
 from utils.utils import get_output_path, setup_logger
@@ -16,11 +18,14 @@ app.conf.task_track_started = True
 app.conf.result_expires = 86400
 
 
+@worker_process_init.connect
+def setup_logger_on_worker(**kwargs):
+    setup_logger(f"worker_{datetime.now().strftime('%Y-%m-%d--%H-%M-%S')}")
+
+
 @app.task(bind=True)
 def model_generation_task(self, ifc_version, name, polygon, project_origin):
-    setup_logger(f"task_{self.request.id}")
     logger = logging.getLogger(__name__)
-
     try:
         logger.info(f"Task {self.request.id}: Starting model generation")
         model_generator = ModelGenerator()
