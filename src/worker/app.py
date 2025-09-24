91ef5cd1c3778726663be623cdcdf3bd354fcd31
Joel Gschwind <joel.gschwind@fhnw.ch>
2025-09-24 17:07:46 +0200

    fix bug (language)

--- src/worker/app.py
+++ src/worker/app.py
@@ -6,6 +6,7 @@ from celery.signals import worker_process_init
 from datetime import datetime
 from core.ifc.model.ifc_version import IfcVersion
 from core.model_generator import ModelGenerator
+from i18n.language import Language
 from utils.utils import get_output_path, setup_logger
 from config.configuration import config
 
@@ -31,6 +32,7 @@ def model_generation_task(self, ifc_version, name, polygon, project_origin, lang
         logger.info(f"Task {self.request.id}: Starting model generation")
         model_generator = ModelGenerator()
         model = model_generator.generate(IfcVersion(ifc_version), name, polygon, project_origin)
+        language = Language(language) if language else None
         ifc_file = model.map_to_ifc(language)
         output_path = get_output_path(self.request.id)
         ifc_file.write(output_path)
